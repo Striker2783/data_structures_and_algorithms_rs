8aeb8259b0125ff5872df11840cf95b0ae45e2ba
striker2783 <owenxingli@gmail.com>
2024-03-03 16:22:02 -0500

    Gave up on arrays because vecs are better

--- src/data_structures/array.rs
+++ src/data_structures/array.rs
@@ -1,50 +1,56 @@
-use std::ops::{Index, IndexMut};
+// Just use a vector, this is shit.
 
-#[derive(Debug, Default)]
-pub struct VectorOwn<T> {
-    // A vector because fuck allocations
-    arr: Vec<T>,
-    size: usize,
-    index: usize,
-}
-impl<T> VectorOwn<T> {
-    pub fn new(size: usize) -> Self {
-        Self {
-            arr: Vec::with_capacity(size),
-            index: 0,
-            size,
-        }
-    }
-    pub fn append(&mut self, e: T) {
-        //
-    }
-}
-impl<T: PartialEq> PartialEq<VectorOwn<T>> for VectorOwn<T> {
-    fn eq(&self, other: &VectorOwn<T>) -> bool {
-        if self.index != other.index {
-            return false;
-        }
-        for i in 0..self.index {
-            if self.arr[i] != other.arr[i] {
-                return false;
-            }
-        }
-        true
-    }
-}
-impl<T> Index<usize> for VectorOwn<T> {
-    type Output = T;
+// use std::ops::{Index, IndexMut};
 
-    fn index(&self, index: usize) -> &Self::Output {
-        &self.arr[index]
-    }
-}
-impl<T> IndexMut<usize> for VectorOwn<T> {
-    fn index_mut(&mut self, index: usize) -> &mut Self::Output {
-        &mut self.arr[index]
-    }
-}
-#[cfg(test)]
-mod tests {
-    //
-}
+// #[derive(Debug, Default)]
+// pub struct VectorOwn<T> {
+//     // A vector because fuck allocations
+//     arr: Vec<T>,
+//     size: usize,
+//     index: usize,
+// }
+// impl<T: Sized> VectorOwn<T> {
+//     pub fn new(size: usize) -> Self {
+//         Self {
+//             arr: {
+//                 let mut vec = Vec::with_capacity(size);
+//                 vec.push(T::default());
+//                 vec
+//             },
+//             index: 0,
+//             size,
+//         }
+//     }
+//     fn resize(&mut self, size: usize) {
+//         self.arr.resize_with(size, Default::default());
+//     }
+//     pub fn append(&mut self, e: T) {
+//         if self.index == self.size {
+//             self.resize(self.size * 2);
+//         }
+//     }
+// }
+// impl<T: PartialEq> PartialEq<VectorOwn<T>> for VectorOwn<T> {
+//     fn eq(&self, other: &VectorOwn<T>) -> bool {
+//         if self.index != other.index {
+//             return false;
+//         }
+//         !(0..self.index).any(|i| self.arr[i] != other.arr[i])
+//     }
+// }
+// impl<T> Index<usize> for VectorOwn<T> {
+//     type Output = T;
+
+//     fn index(&self, index: usize) -> &Self::Output {
+//         &self.arr[index]
+//     }
+// }
+// impl<T> IndexMut<usize> for VectorOwn<T> {
+//     fn index_mut(&mut self, index: usize) -> &mut Self::Output {
+//         &mut self.arr[index]
+//     }
+// }
+// #[cfg(test)]
+// mod tests {
+//     //
+// }
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use data_structures::data_structures::array;
+
 fn main() {
     println!("Hello, world!");
 }
